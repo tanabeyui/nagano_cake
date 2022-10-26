c9af5778e7e23fdde071b9ee702c24b8a2d2338b
EC2 Default User <ec2-user@ip-172-31-47-99.ap-northeast-1.compute.internal>
2022-10-26 09:16:41 +0000

    [Add] データベース作成

---
+++ db/migrate/20221026090533_create_orders.rb
@@ -0,0 +1,15 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id,              null: false, default: ""
+      t.string :name,                      null: false, default: ""
+      t.string :postal_code,               null: false, default: ""
+      t.string :address,                   null: false, default: ""
+      t.integer :postage,              null: false, default: ""
+      t.integer :payment_price,              null: false, default: ""
+      t.integer :payment_method,              null: false, default: "0"
+      t.integer :order_status,              null: false, default: "0"
+      t.timestamps
+    end
+  end
+end
