d39958ada3bdf52b45204a6f5e140419a3cbbe33
EC2 Default User <ec2-user@ip-172-31-47-99.ap-northeast-1.compute.internal>
2022-10-30 16:20:31 +0000

    [Add] 商品一覧・詳細・編集ページの追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,44 @@
 class Admin::ItemsController < ApplicationController
-  def index
-  end
-
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
-  def edit
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
+
+  def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
+
+  def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :genre_id, :price, :is_active)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
+
   has_one_attached :image
+  belongs_to :genre
+
+  enum is_active: { on_sale: true, stop_sell: false }
+  
+  
+  def get_image(width, height)
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
