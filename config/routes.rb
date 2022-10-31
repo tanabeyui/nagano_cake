96b106eb9b663b9483971be4a93d2f76a0739825
EC2 Default User <ec2-user@ip-172-31-47-99.ap-northeast-1.compute.internal>
2022-10-31 09:16:11 +0000

    [Add] customerコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  
+
   namespace :admin do
     resources :genres, only: [:create, :index, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
-  
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
