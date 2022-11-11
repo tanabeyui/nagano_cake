0223b76447e77c550d1f671bf013d57d7f1bdc07
EC2 Default User <ec2-user@ip-172-31-47-99.ap-northeast-1.compute.internal>
2022-11-11 13:29:46 +0000

    [Add] ログイン前アクセス制限の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_customer!, if: :customer_auth?
+  before_action :authenticate_admin!, except: [:index]
+
+
+  private
+
+  def customer_auth?
+    unless controller_name == 'items' || controller_name == 'homes'
+      true
+    end
+  end
 end
