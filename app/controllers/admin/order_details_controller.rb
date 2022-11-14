146a110bccd20f575801743610b6d52423d23f9e
EC2 Default User <ec2-user@ip-172-31-47-99.ap-northeast-1.compute.internal>
2022-11-14 12:24:54 +0000

    [Fix] 製作ステータスと注文ステータスの紐づけ修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,39 +2,28 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
-    @order_detail = OrderDetail.find(params[:id])
+    @order_detail = OrderDetail.find(params[:order_detail][:order_detail_id])
+    @order = @order_detail.order
+
 
     if @order_detail.order.order_status == "wait_payment"
       flash[:danger] = "入金を確認してください！"
-      redirect_to admin_order_path(@order_detail.order_id)
-      return 0
-    elsif @order_detail.order.order_status == "shipping_prepare" || @order_detail.order.order_status == "shipping_finish"
-      flash[:danger] = "製作は完了しています！操作できません！"
-      redirect_to admin_order_path(@order_detail.order_id)
-      return 0
-    end
-
-    if params[:order_detail][:making_status] == "making"
+      redirect_to admin_order_path(@order.id)
+    elsif params[:order_detail][:making_status] == "making"
       @order_detail.update(order_detail_params)
       @order_detail.order.update(order_status: 2)
-      flash[:success] = "製作ステータスを更新しました！製作を開始してください！"
-      redirect_to admin_order_path(@order_detail.order_id)
-
-    elsif params[:order_detail][:making_status] == "making_finish" # && すべて"making_finish"の場合に変える
-      @order_detail.update(order_detail_params)
-      @order_detail.order.update(order_status: 3)
-      flash[:warning] = "製作お疲れ様でした！発送の準備をしてください！"
-      redirect_to admin_order_path(@order_detail.order_id)
-
-    elsif params[:order_detail][:making_status] == "waiting_making"
-      @order_detail.update(order_detail_params)
-      flash[:danger] = "製作ステータスを更新しました！製作を開始してください！"
-      redirect_to admin_order_path(@order_detail.order_id)
-
+      flash[:warning] = "製作ステータスを更新しました！製作を開始してください！"
+      redirect_to admin_order_path(@order.id)
     else
-      flash[:danger] = "製作ステータスを変更できません！製作を開始してください！"
-      redirect_to admin_order_path(@order_detail.order_id)
+      @order_detail.update(order_detail_params)
+      flash[:warning] = "製作ステータスを更新しました！"
+      redirect_to admin_order_path(@order.id)
     end
+
+    if @order.order_details.count == @order.order_details.where(making_status: "making_finish").count
+      @order_detail.order.update(order_status: 3)
+    end
+
   end
 
 
@@ -46,10 +35,3 @@ class Admin::OrderDetailsController < ApplicationController
   end
 
 end
-
-    # if @order_detail.update(order_detail_params)
-      # flash[:success] = "製作ステータスを更新しました！"
-      # redirect_to admin_order_path(@order_detail.order_id)
-    # else
-      # redirect_to about_path
-    # end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,40 +3,23 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @total = 0
-    @order.order_details.each do |order_detail|
-      order_detail.subtotal
-      @total += order_detail.subtotal
-    end
   end
 
   def update
     @order = Order.find(params[:id])
 
-    if params[:order][:order_status] == "wait_payment"
-      @order.update(order_params)
-      flash[:danger] = "注文ステータスを更新しました！入金再度確認してください！"
-      redirect_to admin_order_path(@order)
-
-    elsif params[:order][:order_status] == "confirm_payment"
+    if params[:order][:order_status] == "confirm_payment"
       @order.update(order_params)
         @order.order_details.each do |order_detail|
           order_detail.update(making_status: 1)
         end
       flash[:warning] = "入金を確認しました！製作を開始してください！"
       redirect_to admin_order_path(@order)
-
-    elsif params[:order][:order_status] == "shipping_finish"  # && すべてのorder_detail.making_statusが"making_finish"の場合
+    elsif params[:order][:order_status] == "shipping_finish" && @order.order_details.count == @order.order_details.where(making_status: "making_finish").count
       @order.update(order_params)
-      flash[:warning] = "発送しました！お疲れ様でした！"
+      flash[:success] = "発送しました！お疲れ様でした！"
       redirect_to admin_order_path(@order)
-
     else
-      @total = 0
-        @order.order_details.each do |order_detail|
-          order_detail.subtotal
-          @total += order_detail.subtotal
-        end
       flash[:danger] = "操作できません！製作ステータスを確認してください！"
       render :show
     end
@@ -51,11 +34,3 @@ class Admin::OrdersController < ApplicationController
   end
 
 end
-
-
-    # if @order.update(order_params)
-      # flash[:success] = "注文ステータスを更新しました！"
-      # redirect_to admin_order_path(@order)
-    # else
-      # render :show
-    # end
